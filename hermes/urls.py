9b2e677df7ca387ca3a549dd509088f77994c606
Andrew Kenyon <ak@loyaltyangels.com>
2015-09-16 14:04:16 +0100

    user endpoint change to plural

--- hermes/urls.py
+++ hermes/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^user/', include('user.urls')),
+    url(r'^users/', include('user.urls')),
 ]
