6a393b494924dda17be9cdb12ab0d26101a8a9d0
Andrew Kenyon <ak@loyaltyangels.com>
2015-09-17 14:32:16 +0100

    Created a factory for user profile generation

--- user/tests/factories.py
+++ user/tests/factories.py
@@ -1,4 +1,5 @@
 import factory
+from factory.fuzzy import FuzzyAttribute
 from user import models
 from faker import Factory
 
@@ -10,7 +11,27 @@ class UserFactory(factory.DjangoModelFactory):
     class Meta:
         model = models.CustomUser
 
-    email = fake.email()
+    email = FuzzyAttribute(fake.email)
     is_active = True
     is_staff = False
+
+
+class UserProfileFactory(factory.Factory):
+    class Meta:
+        model = models.UserDetail
+
+    user = factory.SubFactory(UserFactory)
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    date_of_birth = fake.date()
+    phone = fake.phone_number()
+    address_line_1 = fake.street_address()
+    address_line_2 = fake.city()
+    city = fake.city()
+    region = fake.state()
+    postcode = fake.postcode()
+    country = 'United Kingdom'
+    notifications = '0'
+    pass_code = '1234'
+    currency = 'GBP'
 
