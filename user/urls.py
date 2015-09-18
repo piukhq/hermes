0481323aa2cf6a01156caa3a12e4941627bb1679
Andrew Kenyon <ak@loyaltyangels.com>
2015-09-18 17:13:15 +0100

    added authenticate and scheme_accounts url in /users

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
-from user.views import Users, Register, Login
+from user.views import Users, Register, Login, Authenticate, SchemeAccounts
 
 urlpatterns = patterns('user',
-                       url(r'register/$', Register.as_view(), name='user_detail'),
-                       url(r'login/$', Login.as_view(), name='user_detail'),
+                       url(r'authenticate/(?P<uid>[\w-]+)/$', Authenticate.as_view(), name='authenticate_user'),
+                       url(r'register/$', Register.as_view(), name='register_user'),
+                       url(r'login/$', Login.as_view(), name='login'),
+                       url(r'scheme_accounts/$', SchemeAccounts.as_view(), name='user_detail'),
                        url(r'^(?P<uid>[\w-]+)/$', Users.as_view(), name='user_detail'),
+
 )
