9e7fe01ad5e41325a75cb1b2b75d42b25c46fc7b
Andrew Kenyon <ak@loyaltyangels.com>
2015-09-23 17:18:22 +0100

    scheme views now require authentication

--- scheme/tests/tests.py
+++ scheme/tests/tests.py
@@ -12,6 +12,9 @@ class TestScheme(APITestCase):
         cls.scheme_account_question = factories.SchemeAccountSecurityQuestionFactory()
         cls.scheme_account = cls.scheme_account_question.scheme_account
         cls.user = cls.scheme_account.user
+        cls.auth_headers = {
+            'HTTP_AUTHORIZATION': str(cls.user.uid),
+        }
         cls.scheme = cls.scheme_account.scheme
         super(TestScheme, cls).setUpClass()
 
@@ -30,7 +33,7 @@ class TestScheme(APITestCase):
         self.assertEqual(response.data['id'], self.scheme.id)
 
     def test_get_schemes_accounts(self):
-        response = self.client.get('/schemes/accounts/{0}'.format(self.scheme_account.id))
+        response = self.client.get('/schemes/accounts/{0}'.format(self.scheme_account.id), **self.auth_headers)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(type(response.data), ReturnDict)
@@ -43,30 +46,30 @@ class TestScheme(APITestCase):
                 'user': self.user.id,
                 'membership_number': '4786',
                 'card_number': '6011687956185350'}
-        response = self.client.post('/schemes/accounts/', data=data)
+        response = self.client.post('/schemes/accounts/', data=data, **self.auth_headers)
+        self.assertEqual(response.status_code, 201)
         decoded_password = AESCipher(settings.AES_KEY.encode()).decrypt(response.data['password'])
 
-        self.assertEqual(response.status_code, 201)
         self.assertNotEqual(data["password"], response.data['password'])
         self.assertEqual(data["password"], decoded_password)
         self.assertIn('created', response.data)
 
     def test_patch_schemes_accounts(self):
         data = {'card_number': 'new-card-number'}
-        response = self.client.patch('/schemes/accounts/{0}'.format(self.scheme_account.id), data=data)
+        response = self.client.patch('/schemes/accounts/{0}'.format(self.scheme_account.id), data=data, **self.auth_headers)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(type(response.data), ReturnDict)
         self.assertEqual(response.data['card_number'], 'new-card-number')
 
     def test_delete_schemes_accounts(self):
-        response = self.client.delete('/schemes/accounts/{0}'.format(self.scheme_account.id))
+        response = self.client.delete('/schemes/accounts/{0}'.format(self.scheme_account.id), **self.auth_headers)
         deleted_scheme_account = SchemeAccount.objects.get(id=self.scheme_account.id)
 
         self.assertEqual(response.status_code, 204)
         self.assertEqual(deleted_scheme_account.status, SchemeAccount.DELETED)
 
-        response = self.client.get('/schemes/accounts/{0}'.format(self.scheme_account.id))
+        response = self.client.get('/schemes/accounts/{0}'.format(self.scheme_account.id), **self.auth_headers)
         self.assertEqual(response.status_code, 404)
 
     def test_post_schemes_account_question(self):
@@ -75,13 +78,13 @@ class TestScheme(APITestCase):
             "question": "City of birth?",
             "answer": "London"
         }
-        response = self.client.post('/schemes/accounts/questions/', data=data)
+        response = self.client.post('/schemes/accounts/questions/', data=data, **self.auth_headers)
         self.assertEqual(response.status_code, 201)
         self.assertNotEqual(response.data["answer"], data["answer"])
         self.assertIn("id", response.data)
 
     def test_get_schemes_account_question(self):
-        response = self.client.get('/schemes/accounts/questions/{0}'.format(self.scheme_account_question.id))
+        response = self.client.get('/schemes/accounts/questions/{0}'.format(self.scheme_account_question.id), **self.auth_headers)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(type(response.data), ReturnDict)
--- scheme/views.py
+++ scheme/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from scheme.models import Scheme, SchemeAccount, SchemeAccountSecurityQuestion
 from scheme.serializers import SchemeSerializer, SchemeAccountSerializer, SchemeAccountQuestionSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from user.authenticators import UIDAuthentication
 
 
 class SchemesList(generics.ListAPIView):
@@ -17,6 +19,9 @@ class RetrieveScheme(RetrieveAPIView):
 
 
 class RetrieveUpdateDeleteAccount(RetrieveUpdateAPIView):
+    authentication_classes = (UIDAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
     serializer_class = SchemeAccountSerializer
     queryset = SchemeAccount.active_objects
 
@@ -28,13 +33,22 @@ class RetrieveUpdateDeleteAccount(RetrieveUpdateAPIView):
 
 
 class CreateAccount(CreateAPIView):
+    authentication_classes = (UIDAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
     serializer_class = SchemeAccountSerializer
 
 
 class CreateQuestion(CreateAPIView):
+    authentication_classes = (UIDAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
     serializer_class = SchemeAccountQuestionSerializer
 
 
 class RetrieveUpdateDestroyQuestion(RetrieveUpdateDestroyAPIView):
+    authentication_classes = (UIDAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
     serializer_class = SchemeAccountQuestionSerializer
     queryset = SchemeAccountSecurityQuestion.objects
