297df6a3128936a814115f12b11200ec46e754c9
Ben Olsen <bo@loyaltyangels.com>
2015-10-12 18:15:46 +0100

    update facebook auth endpoints

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
-from user.views import Users, Register, Login, Authenticate, RetrieveSchemeAccount, FaceBookLogin
+from user.views import Users, Register, Login, Authenticate, RetrieveSchemeAccount, FaceBookLogin, FaceBookLoginWeb
 
 urlpatterns = patterns('user',
                        url(r'authenticate/?$', Authenticate.as_view(), name='authenticate_user'),
-                       url(r'auth/facebook/?$', FaceBookLogin.as_view(), name='authenticate_user'),
+                       url(r'auth/facebook/?$', FaceBookLogin.as_view(), name='auth_facebook'),
+                       url(r'auth/facebook-web/?$', FaceBookLoginWeb.as_view(), name='auth_facebook_web'),
                        url(r'register/?$', Register.as_view(), name='register_user'),
                        url(r'login/?$', Login.as_view(), name='login'),
                        url(r'scheme_accounts/(?P<scheme_account_id>[0-9]+)/?$', RetrieveSchemeAccount.as_view(), name='user_detail'),
