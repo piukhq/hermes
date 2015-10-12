7e2295a861c9c6b391dea7a9a5c6a3e39691953e
Ben Olsen <bo@loyaltyangels.com>
2015-10-12 10:19:34 +0100

    update scheme urls

--- scheme/urls.py
+++ scheme/urls.py
@@ -3,9 +3,9 @@ from scheme.views import (CreateAccount, SchemesList, RetrieveUpdateDeleteAccoun
                             CreateAnswer, RetrieveUpdateDestroyAnswer)
 
 urlpatterns = patterns('schemes',
-                       url(r'accounts/?$', CreateAccount.as_view(), name='create_scheme_account'),
-                       url(r'accounts/(?P<pk>[0-9]+)$', RetrieveUpdateDeleteAccount.as_view(), name='retrieve_account'),
-                       url(r'^$', SchemesList.as_view(), name='list_schemes'),
-                       url(r'^(?P<pk>[0-9]+)$', RetrieveScheme.as_view(), name='retrieve_scheme'),
-                       url(r'^accounts/credentials', CreateAnswer.as_view(), name='create_question'),
+                       url(r'^/accounts/?$', CreateAccount.as_view(), name='create_scheme_account'),
+                       url(r'^/accounts/(?P<pk>[0-9]+)$', RetrieveUpdateDeleteAccount.as_view(), name='retrieve_account'),
+                       url(r'^/$', SchemesList.as_view(), name='list_schemes'),
+                       url(r'^/(?P<pk>[0-9]+)$', RetrieveScheme.as_view(), name='retrieve_scheme'),
+                       url(r'^/accounts/credentials', CreateAnswer.as_view(), name='create_question'),
 )
