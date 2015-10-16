a9cee901471eee6f96c3369929623e457a08fd31
Ben Olsen <bo@loyaltyangels.com>
2015-10-16 17:02:44 +0100

    refactor auth

--- hermes/settings.py
+++ hermes/settings.py
@@ -93,6 +93,16 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'user.authentication.JwtAuthentication',
+    )
+}
+
+
 WSGI_APPLICATION = 'hermes.wsgi.application'
 
 
--- payment_card/views.py
+++ payment_card/views.py
@@ -1,25 +1,17 @@
 from rest_framework import generics
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from payment_card.models import PaymentCardAccount, PaymentCard
 from payment_card.serializers import PaymentCardAccountSerializer, PaymentCardSerializer
 from rest_framework import status
-from user.authenticators import JwtAuthentication
 
 
 class ListPaymentCard(generics.ListAPIView):
-    authentication_classes = (JwtAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
     queryset = PaymentCard.objects
     serializer_class = PaymentCardSerializer
 
 
 class RetrievePaymentCardAccount(RetrieveUpdateAPIView):
-    authentication_classes = (JwtAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
     queryset = PaymentCardAccount.active_objects
     serializer_class = PaymentCardAccountSerializer
 
@@ -31,7 +23,4 @@ class RetrievePaymentCardAccount(RetrieveUpdateAPIView):
 
 
 class CreatePaymentCardAccount(CreateAPIView):
-    authentication_classes = (JwtAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
     serializer_class = PaymentCardAccountSerializer
