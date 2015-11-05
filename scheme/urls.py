e099ebf9c1734c18af6378d742cce800e834ac02
ml <ml@loyaltyangels.com>
2015-11-05 13:35:00 +0000

    Merge branch 'master' into hermes_status_snapshot
    Conflicts:
    	scheme/tests/test_scheme_accounts.py
    	scheme/urls.py

--- scheme/urls.py
+++ scheme/urls.py
@@ -16,6 +16,6 @@ urlpatterns = patterns('schemes',
                            name='retrieve_account'),
                        url(r'^/?$', SchemesList.as_view(), name='list_schemes'),
                        url(r'^/(?P<pk>[0-9]+)$', RetrieveScheme.as_view(), name='retrieve_scheme'),
-                       url(r'^/accounts/(?P<pk>[0-9]+)/credentials', LinkCredentials.as_view(), name='create_question'),
+                       url(r'^/accounts/(?P<pk>[0-9]+)/link', LinkCredentials.as_view(), name='create_question'),
                        url(r'^/dashboard', SchemeAccountStatusData.as_view(), name='schemes_status'),
 )
