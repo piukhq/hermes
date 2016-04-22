d1a7623fdf92a394a0c0debed3efd4f404d86538
Chris Latham <cl@loyaltyangels.com>
2016-04-22 11:25:59 +0100

    change test_repr to test_model_description

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -11,7 +11,7 @@ class TestSettings(TestCase):
     def test_value_type_name(self):
         self.assertEqual('number', self.setting.value_type_name)
 
-    def test_repr(self):
+    def test_model_description(self):
         self.assertEqual('(number) test-setting: 10', str(self.setting))
 
 
@@ -23,5 +23,5 @@ class TestUserSettings(TestCase):
         cls.user_setting = UserSetting(user=user, setting=setting, value='5')
         super().setUpClass()
 
-    def test_repr(self):
+    def test_model_description(self):
         self.assertEqual('test@test.com - test-setting: 5', str(self.user_setting))
