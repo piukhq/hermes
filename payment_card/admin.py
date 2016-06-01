4f7394264d2130364afb5c598fad02fa0cc73983
Chris Latham <cl@loyaltyangels.com>
2016-06-01 11:35:14 +0100

    make payment card accounts show more useful information in django admin

--- payment_card/admin.py
+++ payment_card/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from payment_card.models import Issuer, PaymentCardAccount, PaymentCard, PaymentAccountImageCriteria, \
     PaymentCardAccountImage
 
+
+class PaymentCardAccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'payment_card', 'pan_start', 'pan_end', 'is_deleted')
+
+
 admin.site.register(Issuer)
-admin.site.register(PaymentCardAccount)
+admin.site.register(PaymentCardAccount, PaymentCardAccountAdmin)
 admin.site.register(PaymentCard)
 admin.site.register(PaymentCardAccountImage)
 admin.site.register(PaymentAccountImageCriteria)
