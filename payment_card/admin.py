0244bd174ce0c7d58a4a9d0f83648d7526974c9b
Chris Latham <cl@loyaltyangels.com>
2016-06-09 16:19:17 +0100

    use two-panel layout for payment card account image criterias in django admin

--- payment_card/admin.py
+++ payment_card/admin.py
@@ -7,8 +7,13 @@ class PaymentCardAccountAdmin(admin.ModelAdmin):
     list_display = ('user', 'payment_card', 'pan_start', 'pan_end', 'is_deleted')
 
 
+class PaymentAccountImageCriteriaAdmin(admin.ModelAdmin):
+    model = PaymentAccountImageCriteria
+    filter_horizontal = ('payment_card_accounts',)
+
+
 admin.site.register(Issuer)
 admin.site.register(PaymentCardAccount, PaymentCardAccountAdmin)
 admin.site.register(PaymentCard)
 admin.site.register(PaymentCardAccountImage)
-admin.site.register(PaymentAccountImageCriteria)
+admin.site.register(PaymentAccountImageCriteria, PaymentAccountImageCriteriaAdmin)
