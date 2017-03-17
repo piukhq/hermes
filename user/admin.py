79b3bcb91de5ae91ca9e03cdb696aeee09c4587e
olivere <oe@bink.com>
2017-03-17 14:38:08 +0000

    Remove now obsolete admin MarketingCode changes.

--- user/admin.py
+++ user/admin.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.admin import StackedInline
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
-from django.contrib.admin.widgets import AdminDateWidget
 
 from payment_card.models import PaymentCardAccount
 from scheme.models import SchemeAccount
@@ -80,26 +79,5 @@ class UserSettingAdmin(admin.ModelAdmin):
     search_fields = ('user__email', 'setting__slug', 'value')
 
 
-class MarketingCodeAdminForm(forms.ModelForm):
-    code = forms.CharField(max_length=100, required=True)
-    date_from = forms.DateTimeField(widget=AdminDateWidget(), required=True)
-    date_to = forms.DateTimeField(widget=AdminDateWidget(), required=True)
-    description = forms.CharField(max_length=300)
-    partner = forms.CharField(max_length=100, required=True)
-
-
-class MarketingCodeAdmin(admin.ModelAdmin):
-    form = MarketingCodeAdminForm
-    fields = ['code', 'date_from', 'date_to', 'description', 'partner', ]
-
-    def save_model(self, request, obj, form, change):
-        obj.code = request.POST.get('code', '').lower()
-        obj.date_from = request.POST.get('date_from', '')
-        obj.date_to = request.POST.get('date_to', '')
-        obj.description = request.POST.get('description', '')
-        obj.partner = request.POST.get('partner', '')
-        obj.save()
-
-
 admin.site.register(Setting)
-admin.site.register(MarketingCode, MarketingCodeAdmin)
+admin.site.register(MarketingCode)
