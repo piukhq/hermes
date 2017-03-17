c4ec05ba504750fccdf0ee6bfeec0dd7788f64ca
olivere <oe@bink.com>
2017-03-17 14:34:13 +0000

    Fix Flake8 carriage return errors.

--- user/models.py
+++ user/models.py
@@ -46,10 +46,12 @@ class ModifyingFieldDescriptor(object):
     """ Modifies a field when set using the field's (overriden) .to_python() method. """
     def __init__(self, field):
         self.field = field
+
     def __get__(self, instance, owner=None):
         if instance is None:
             raise AttributeError('Can only be accessed via an instance.')
         return instance.__dict__[self.field.name]
+
     def __set__(self, instance, value):
         instance.__dict__[self.field.name] = self.field.to_python(value)
 
@@ -60,6 +62,7 @@ class LowerCaseCharField(CharField):
         if isinstance(value, str):
             return value.lower()
         return value
+
     def contribute_to_class(self, cls, name):
         super(LowerCaseCharField, self).contribute_to_class(cls, name)
         setattr(cls, self.name, ModifyingFieldDescriptor(self))
