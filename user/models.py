92577987b0f47a2d7cac91360cfa4a5e22499317
Marc Kirkwood <mkirkwood@bink.com>
2017-03-30 11:17:23 +0100

    Fixed ClientApplicationBundle.__str__

--- user/models.py
+++ user/models.py
@@ -131,7 +131,7 @@ class ClientApplicationBundle(models.Model):
         return cls.objects.filter(client_id=BINK_APP_ID)
 
     def __str__(self):
-        return '{} ({})'.format(self.bundle_id, str(self.client))
+        return '{} ({})'.format(self.bundle_id, self.client)
 
 
 class ClientApplicationKit(models.Model):
