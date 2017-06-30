c997ba013d3bd65e6c785dbb03088779f748afb9
olivere <oe@bink.com>
2017-06-30 15:31:03 +0100

    Minor format string fix.

--- intercom/intercom_api.py
+++ intercom/intercom_api.py
@@ -151,7 +151,7 @@ def update_payment_account_custom_attribute(token, account):
         "DEL:{}".format(str(account.is_deleted).lower()),
     )
 
-    key = "Payment Card " + str(account.order)
+    key = "Payment Card {}".format(str(account.order))
 
     return update_user_custom_attribute(token, account.user.uid, key, attr_value)
 
