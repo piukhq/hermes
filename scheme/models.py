dc574a5c95dca0921fff306e80509e163496f5d6
Christian Prior <cprior@bink.com>
2018-05-24 15:26:51 +0100

    fix to required credentials query for missing credentials method

--- scheme/models.py
+++ scheme/models.py
@@ -121,11 +121,11 @@ class Scheme(models.Model):
 
     @property
     def join_questions(self):
-        return self.questions.filter(options=F('options').bitor(1 << 1))
+        return self.questions.filter(options=F('options').bitor(SchemeCredentialQuestion.JOIN))
 
     @property
     def link_questions(self):
-        return self.questions.filter(options=F('options').bitor(1 << 0))
+        return self.questions.filter(options=F('options').bitor(SchemeCredentialQuestion.LINK))
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.company)
@@ -277,7 +277,9 @@ class SchemeAccount(models.Model):
         A scan or manual question is an optional if one of the other exists
         """
         required_credentials = {
-            question.type for question in self.scheme.questions.exclude(options=SchemeCredentialQuestion.JOIN)
+            question.type for question in self.scheme.questions.filter(
+                options__in=[F('options').bitor(SchemeCredentialQuestion.LINK), SchemeCredentialQuestion.NONE]
+            )
         }
         manual_question = self.scheme.manual_question
         scan_question = self.scheme.scan_question
