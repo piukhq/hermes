452dfbb16187cd4f566911acf949c4a9182bed89
Francesco Milani <fmilani@bink.com>
2018-12-06 10:35:30 +0000

    modify organisation check to use environment variable client_id

--- hermes/settings.py
+++ hermes/settings.py
@@ -412,3 +412,6 @@ DATADOG_TRACE = {
     'AGENT_HOSTNAME': env_var('DATADOG_APM_HOST', 'datadog-agent-trace.datadog'),
     'ENABLED': env_var('DATADOG_APM_ENABLED', False)
 }
+
+# Organisation allowed to add already existing scheme accounts (Organisation name used by Barclays in django admin)
+ALLOWED_CLIENT_ID = env_var('ALLOWED_CLIENT_ID', '2zXAKlzMwU5mefvs4NtWrQNDNXYrDdLwWeSCoCCrjd8N0VBHoi')
