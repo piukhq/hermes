0432c8272f59e9d97bdf8112ef6ead8e7a243766
kaziz <kaziz@bink.com>
2019-07-01 15:47:17 +0100

    Add PaymentAudit model

--- payment_card/admin.py
+++ payment_card/admin.py
@@ -145,4 +145,12 @@ class PaymentCardUserAssociationAdmin(admin.ModelAdmin):
     card_is_deleted.boolean = True
 
 
+@admin.register(models.PaymentAudit)
+class PaymentAuditAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'scheme_account', 'transaction_ref', 'transaction_id', 'status', 'created_on', 'modified_on',)
+    search_fields = ('user_id', 'scheme_account', 'scheme_account__scheme__name' 'transaction_ref', 'transaction_id', 'status',)
+    readonly_fields = ('user_id', 'scheme_account', 'transaction_ref', 'transaction_id', 'created_on', 'modified_on',
+                       'void_attempts')
+
+
 admin.site.register(models.Issuer)
