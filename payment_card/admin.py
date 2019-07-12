8d1e4b6e115e96d52755e838381e3e326330e3e0
kaziz <kaziz@bink.com>
2019-07-12 08:56:00 +0100

    flake8

--- payment_card/admin.py
+++ payment_card/admin.py
@@ -147,8 +147,10 @@ class PaymentCardUserAssociationAdmin(admin.ModelAdmin):
 
 @admin.register(models.PaymentAudit)
 class PaymentAuditAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'scheme_account', 'transaction_ref', 'transaction_id', 'status', 'created_on', 'modified_on',)
-    search_fields = ('user_id', 'scheme_account', 'scheme_account__scheme__name' 'transaction_ref', 'transaction_id', 'status',)
+    list_display = ('user_id', 'scheme_account', 'transaction_ref', 'transaction_id', 'status', 'created_on',
+                    'modified_on',)
+    search_fields = ('user_id', 'scheme_account', 'scheme_account__scheme__name' 'transaction_ref', 'transaction_id',
+                     'status',)
     readonly_fields = ('user_id', 'scheme_account', 'transaction_ref', 'transaction_id', 'created_on', 'modified_on',
                        'void_attempts')
 
