fe4c42d5d5b3fe735b0afd3054c6f4d2352ac397
kaziz <kaziz@bink.com>
2019-07-15 16:42:11 +0100

    Encapsulate Spreedly calls into its own module

--- payment_card/payment.py
+++ payment_card/payment.py
@@ -1,13 +1,14 @@
 import logging
+from typing import Optional
 
-import requests
 import sentry_sdk
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.exceptions import APIException
 from tenacity import retry, stop_after_attempt, retry_if_exception_type, wait_exponential
 
-from hermes.settings import SPREEDLY_ENVIRONMENT_KEY, SPREEDLY_ACCESS_SECRET, SPREEDLY_GATEWAY_TOKEN, SPREEDLY_BASE_URL
+from hermes.settings import SPREEDLY_ENVIRONMENT_KEY, SPREEDLY_ACCESS_SECRET, SPREEDLY_GATEWAY_TOKEN
+from hermes.spreedly import Spreedly, SpreedlyError
 from hermes.tasks import RetryTaskStore
 from payment_card.models import PaymentAudit, PaymentStatus, PaymentCardAccount
 from scheme.models import SchemeAccount
@@ -56,73 +57,41 @@ class Payment:
     payment_auth_url = "{}/v1/gateways/{gateway_token}/authorize.json"
     payment_void_url = "{}/v1/transactions/{transaction_token}/void.json"
 
-    def __init__(self, audit_obj: PaymentAudit, amount: int = 0, currency_code: str = 'GBP', payment_token: str = None):
+    def __init__(self, audit_obj: PaymentAudit, amount: Optional[int] = None, currency_code: str = 'GBP',
+                 payment_token: Optional[str] = None):
         self.audit_obj = audit_obj
         self.amount = amount
         self.currency_code = currency_code
         self.payment_token = payment_token
 
-        self.auth_resp = None
-        self.void_resp = None
-        self.transaction_token = None
+        self.spreedly = Spreedly(SPREEDLY_ENVIRONMENT_KEY, SPREEDLY_ACCESS_SECRET, currency_code=currency_code)
 
-    def _auth(self, payment_token: str = None) -> None:
+    def _auth(self, payment_token: Optional[str] = None) -> None:
         p_token = payment_token or self.payment_token
-
         if not p_token:
             raise PaymentError("No payment token provided")
 
-        payload = {
-            "transaction": {
-                "payment_method_token": self.payment_token,
-                "amount": self.amount,
-                "currency_code": self.currency_code,
-                "order_id": self.audit_obj.transaction_ref
-            }
-        }
-
         try:
-            resp = requests.post(
-                self.payment_auth_url.format(SPREEDLY_BASE_URL, gateway_token=SPREEDLY_GATEWAY_TOKEN),
-                json=payload,
-                auth=(SPREEDLY_ENVIRONMENT_KEY, SPREEDLY_ACCESS_SECRET)
+            self.spreedly.authorise(
+                payment_token=self.payment_token,
+                amount=self.amount,
+                order_id=self.audit_obj.transaction_ref,
+                gateway_token=SPREEDLY_GATEWAY_TOKEN
             )
-            self.auth_resp = resp.json()
-            if not self.auth_resp['transaction']['succeeded']:
-                message = "Payment authorisation error - response: {}".format(
-                    self.auth_resp['transaction']['response']
-                )
-                logging.error(message)
-                raise PaymentError("PSP has responded with unsuccessful auth")
-
-            self.transaction_token = self.auth_resp['transaction']['token']
-
-        except requests.RequestException as e:
-            raise PaymentError("Error authorising payment with payment service provider") from e
+        except SpreedlyError as e:
+            raise PaymentError from e
         except KeyError as e:
             raise PaymentError("Error with auth response format") from e
 
-    def _void(self, transaction_token: str = None) -> None:
-        transaction_token = transaction_token or self.transaction_token
-
+    def _void(self, transaction_token: Optional[str] = None) -> None:
+        transaction_token = transaction_token or self.spreedly.transaction_token
         if not transaction_token:
             raise PaymentError("No transaction token provided to void")
 
         try:
-            resp = requests.post(
-                self.payment_void_url.format(SPREEDLY_BASE_URL, transaction_token=transaction_token),
-                auth=(SPREEDLY_ENVIRONMENT_KEY, SPREEDLY_ACCESS_SECRET)
-            )
-
-            self.void_resp = resp.json()
-            if not self.void_resp['transaction']['succeeded']:
-                message = "Payment void error - response: {}".format(
-                    self.void_resp['transaction']['response']
-                )
-                logging.error(message)
-                raise PaymentError("PSP has responded with unsuccessful void")
-        except requests.RequestException as e:
-            raise PaymentError("Error voiding payment with payment service provider") from e
+            self.spreedly.void(transaction_token)
+        except SpreedlyError as e:
+            raise PaymentError from e
         except KeyError as e:
             raise PaymentError("Error with void response format") from e
 
@@ -135,7 +104,7 @@ class Payment:
 
     @staticmethod
     def process_payment_auth(user_id: int, scheme_acc: SchemeAccount, payment_card_id: int,
-                             payment_amount: int = 100) -> None:
+                             payment_amount: int) -> None:
         """
         Starts an audit trail and authorises a payment.
         Any failure to authorise a payment will cause the join to fail.
@@ -155,14 +124,14 @@ class Payment:
            wait=wait_exponential(min=2, max=8),
            reraise=True)
     def attempt_auth(payment_audit: PaymentAudit, payment_card_id: int,
-                     payment_amount: int = 100) -> None:
+                     payment_amount: int) -> None:
         try:
             pcard_account = PaymentCardAccount.objects.get(pk=payment_card_id)
             payment = Payment(audit_obj=payment_audit, amount=payment_amount, payment_token=pcard_account.psp_token)
 
             payment._auth()
 
-            payment_audit.transaction_token = payment.transaction_token
+            payment_audit.transaction_token = payment.spreedly.transaction_token
             payment_audit.status = PaymentStatus.AUTHORISED
             payment_audit.save()
         except PaymentError as e:
