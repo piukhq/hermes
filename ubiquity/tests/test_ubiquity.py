f64297e251961478fec8713c5022551f2ed0f5ed
Christian Prior <cprior@bink.com>
2019-07-16 14:39:05 +0100

    remove previous balance when replacing membership card with a put request

--- ubiquity/tests/test_ubiquity.py
+++ ubiquity/tests/test_ubiquity.py
@@ -899,6 +899,45 @@ class TestResources(APITestCase):
         self.assertIsNone(answers.get(self.put_scheme_manual_q.type))
 
     @patch('scheme.mixins.analytics', autospec=True)
+    @patch('ubiquity.views.async_link', autospec=True)
+    @patch('ubiquity.serializers.async_balance', autospec=True)
+    @patch('ubiquity.views.async_balance', autospec=True)
+    @patch.object(MembershipTransactionsMixin, '_get_hades_transactions')
+    def test_membership_card_put_with_previous_balance(self, *_):
+        scheme_account = SchemeAccountFactory(scheme=self.put_scheme)
+        SchemeAccountEntryFactory(scheme_account=scheme_account, user=self.user)
+        SchemeCredentialAnswerFactory(question=self.put_scheme_manual_q, scheme_account=scheme_account, answer='9999')
+        SchemeCredentialAnswerFactory(question=self.put_scheme_auth_q, scheme_account=scheme_account, answer='pass')
+        scheme_account.balances = [{"points": 1, "scheme_account_id": 27308}]
+        scheme_account.save()
+
+        payload = {
+            "membership_plan": self.put_scheme.id,
+            "account": {
+                "add_fields": [
+                    {
+                        "column": "card_number",
+                        "value": "test12345678"
+                    }
+                ],
+                "authorise_fields": [
+                    {
+                        "column": "password",
+                        "value": "pass"
+                    }
+                ]
+            }
+        }
+
+        resp_put = self.client.put(reverse('membership-card', args=[scheme_account.id]), data=json.dumps(payload),
+                                   content_type='application/json', **self.auth_headers)
+
+        self.assertEqual(resp_put.status_code, 200)
+        scheme_account.refresh_from_db()
+        self.assertEqual(scheme_account.status, SchemeAccount.PENDING)
+        self.assertFalse(scheme_account.balances)
+
+    @patch('scheme.mixins.analytics', autospec=True)
     @patch('ubiquity.serializers.async_balance', autospec=True)
     @patch('ubiquity.views.async_balance', autospec=True)
     @patch('ubiquity.views.async_registration', autospec=True)
