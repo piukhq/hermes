332af63abf65fac177b0166290e99b59c9b7866f
kaziz <kaziz@bink.com>
2019-07-18 12:20:14 +0100

    Add string representation of PaymentAudit objects.

--- payment_card/models.py
+++ payment_card/models.py
@@ -263,3 +263,8 @@ class PaymentAudit(models.Model):
     void_attempts = models.IntegerField(default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'PaymentAudit id: {} - User id: {} - SchemeAccount id: {}'.format(
+            self.id, self.user_id, self.scheme_account_id
+        )
