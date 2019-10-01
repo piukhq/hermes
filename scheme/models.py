f08ff2e2eeb06e8612e9e4920099c4b3ca94d108
mmarsh <mmarsh@bink.com>
2019-10-01 04:25:02 +0100

    Remove from hermes.traced_requests import requests in config line of scheme/modles did not seem to be in Dev. Looks like history of this branch may have included extra commits

--- scheme/models.py
+++ scheme/models.py
@@ -23,7 +23,6 @@ from django.utils import timezone
 from analytics.api import update_scheme_account_attribute_new_status, update_scheme_account_attribute
 from common.models import Image
 from hermes.settings import TO_DAEDALUS, ENABLE_DAEDALUS_MESSAGING
-from hermes.traced_requests import requests
 from scheme.credentials import BARCODE, CARD_NUMBER, CREDENTIAL_TYPES, ENCRYPTED_CREDENTIALS
 from scheme.encyption import AESCipher
 
