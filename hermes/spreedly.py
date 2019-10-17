3a53d47a7a2103060eb32b071955646b6eed24c4
Chris Latham <backwardspy@gmail.com>
2019-10-17 10:39:47 +0100

    flake8

--- hermes/spreedly.py
+++ hermes/spreedly.py
@@ -3,6 +3,7 @@ import logging
 import requests
 from django.conf import settings
 
+
 class SpreedlyError(Exception):
     pass
 
@@ -11,7 +12,7 @@ class Spreedly:
     payment_auth_url = "{}/v1/gateways/{gateway_token}/authorize.json"
     payment_void_url = "{}/v1/transactions/{transaction_token}/void.json"
 
-    def __init__(self, environment_key: str, access_secret: str, currency_code: str = 'GBP'):
+    def __init__(self, environment_key: str, access_secret: str, currency_code: str = "GBP"):
         self.environment_key = environment_key
         self.access_secret = access_secret
         self.currency_code = currency_code
@@ -26,24 +27,22 @@ class Spreedly:
                 "payment_method_token": payment_token,
                 "amount": amount,
                 "currency_code": self.currency_code,
-                "order_id": order_id
+                "order_id": order_id,
             }
         }
         try:
             resp = requests.post(
                 self.payment_auth_url.format(settings.SPREEDLY_BASE_URL, gateway_token=gateway_token),
                 json=payload,
-                auth=(self.environment_key, self.access_secret)
+                auth=(self.environment_key, self.access_secret),
             )
             self.auth_resp = resp.json()
-            if not self.auth_resp['transaction']['succeeded']:
-                message = "Payment authorisation error - response: {}".format(
-                    self.auth_resp['transaction']['response']
-                )
+            if not self.auth_resp["transaction"]["succeeded"]:
+                message = "Payment authorisation error - response: {}".format(self.auth_resp["transaction"]["response"])
                 logging.error(message)
                 raise SpreedlyError("Spreedly has responded with unsuccessful auth")
 
-            self.transaction_token = self.auth_resp['transaction']['token']
+            self.transaction_token = self.auth_resp["transaction"]["token"]
 
         except requests.RequestException as e:
             raise SpreedlyError("Error authorising payment with Spreedly") from e
@@ -54,14 +53,12 @@ class Spreedly:
         try:
             resp = requests.post(
                 self.payment_void_url.format(settings.SPREEDLY_BASE_URL, transaction_token=transaction_token),
-                auth=(self.environment_key, self.access_secret)
+                auth=(self.environment_key, self.access_secret),
             )
 
             self.void_resp = resp.json()
-            if not self.void_resp['transaction']['succeeded']:
-                message = "Payment void error - response: {}".format(
-                    self.void_resp['transaction']['response']
-                )
+            if not self.void_resp["transaction"]["succeeded"]:
+                message = "Payment void error - response: {}".format(self.void_resp["transaction"]["response"])
                 logging.error(message)
                 raise SpreedlyError("Spreedly has responded with unsuccessful void")
         except requests.RequestException as e:
