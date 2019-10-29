752f6da73f615c3b790188cd1243433ae4439890
kaziz <kaziz@bink.com>
2019-10-29 10:31:40 +0000

    Refactor Permit.__init__ to reduce complexity

--- hermes/channels.py
+++ hermes/channels.py
@@ -50,6 +50,9 @@ class Permit:
         if bundle_id == settings.INTERNAL_SERVICE_BUNDLE:
             self.service_allow_all = True
 
+        self._authenticate_bundle(organisation_name, bundle_id)
+
+    def _authenticate_bundle(self, organisation_name, bundle_id):
         if not self.client and not organisation_name and not self.service_allow_all:
             raise exceptions.AuthenticationFailed('Invalid Token')
         elif organisation_name and not self.client:
