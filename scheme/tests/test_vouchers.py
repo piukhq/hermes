df087eda59f82f3117c00cf669f6f9a49f7394fa
Chris Latham <backwardspy@gmail.com>
2019-11-18 15:14:05 +0000

    add earn prefix & currency to voucher response

--- scheme/tests/test_vouchers.py
+++ scheme/tests/test_vouchers.py
@@ -19,11 +19,12 @@ class TestVouchers(TestCase):
             expiry_months=3,
             earn_type=VoucherScheme.EARNTYPE_ACCUMULATOR,
             earn_prefix="£",
+            earn_currency="GBP",
             burn_type=VoucherScheme.BURNTYPE_VOUCHER,
             burn_value=5,
             burn_prefix="£",
-            headline_inprogress="{{earn_prefix}}{{earn_target_remaining|floatformat}} left to go!",
-            headline_issued="{{burn_prefix}}{{burn_value|floatformat}} voucher earned",
+            headline_inprogress="{{earn_prefix}}{{earn_target_remaining|floatformat:2}} left to go!",
+            headline_issued="{{burn_prefix}}{{burn_value|floatformat:2}} voucher earned",
             headline_redeemed="Voucher redeemed",
             headline_expired="Voucher expired",
         )
@@ -32,10 +33,12 @@ class TestVouchers(TestCase):
             barcode_type=0,
             expiry_months=3,
             earn_type=VoucherScheme.EARNTYPE_JOIN,
+            earn_prefix="£",
+            earn_currency="GBP",
             burn_type=VoucherScheme.BURNTYPE_VOUCHER,
             burn_value=5,
             burn_prefix="£",
-            headline_issued="{{burn_prefix}}{{burn_value|floatformat}} voucher earned",
+            headline_issued="{{burn_prefix}}{{burn_value|floatformat:2}} voucher earned",
             headline_redeemed="Voucher redeemed",
             headline_expired="Voucher expired",
         )
@@ -44,13 +47,13 @@ class TestVouchers(TestCase):
         vs = VoucherScheme.objects.get(scheme__slug=TEST_SLUG, earn_type=VoucherScheme.EARNTYPE_ACCUMULATOR)
         headline_template = vs.get_headline(vouchers.VoucherState.IN_PROGRESS)
         headline = vouchers.apply_template(headline_template, voucher_scheme=vs, earn_value=50, earn_target_value=100)
-        self.assertEqual(headline, "£50 left to go!")
+        self.assertEqual(headline, "£50.00 left to go!")
 
     def test_accumulator_issued_headline(self):
         vs = VoucherScheme.objects.get(scheme__slug=TEST_SLUG, earn_type=VoucherScheme.EARNTYPE_ACCUMULATOR)
         headline_template = vs.get_headline(vouchers.VoucherState.ISSUED)
         headline = vouchers.apply_template(headline_template, voucher_scheme=vs, earn_value=50, earn_target_value=100)
-        self.assertEqual(headline, "£5 voucher earned")
+        self.assertEqual(headline, "£5.00 voucher earned")
 
     def test_accumulator_redeemed_headline(self):
         vs = VoucherScheme.objects.get(scheme__slug=TEST_SLUG, earn_type=VoucherScheme.EARNTYPE_ACCUMULATOR)
@@ -68,7 +71,7 @@ class TestVouchers(TestCase):
         vs = VoucherScheme.objects.get(scheme__slug=TEST_SLUG, earn_type=VoucherScheme.EARNTYPE_JOIN)
         headline_template = vs.get_headline(vouchers.VoucherState.ISSUED)
         headline = vouchers.apply_template(headline_template, voucher_scheme=vs, earn_value=50, earn_target_value=100)
-        self.assertEqual(headline, "£5 voucher earned")
+        self.assertEqual(headline, "£5.00 voucher earned")
 
     def test_join_redeemed_headline(self):
         vs = VoucherScheme.objects.get(scheme__slug=TEST_SLUG, earn_type=VoucherScheme.EARNTYPE_JOIN)
@@ -97,7 +100,13 @@ class TestVouchers(TestCase):
         self.assertEqual(
             voucher,
             {
-                "earn": {"type": "accumulator", "value": 300, "target_value": 0},
+                "earn": {
+                    "type": "accumulator",
+                    "prefix": vs.earn_prefix,
+                    "currency": vs.earn_currency,
+                    "value": 300,
+                    "target_value": 0
+                },
                 "burn": {
                     "type": vs.burn_type,
                     "currency": vs.burn_currency,
@@ -108,7 +117,7 @@ class TestVouchers(TestCase):
                 "code": "abc123",
                 "date_issued": now,
                 "expiry_date": arrow.get(now).shift(months=vs.expiry_months).timestamp,
-                "headline": "£5 voucher earned",
+                "headline": "£5.00 voucher earned",
                 "state": "issued",
                 "subtext": "",
             },
