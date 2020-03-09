d94451cbcd7345ca7d3f546f139a5743dd841a77
Francesco Milani <fmilani@bink.com>
2020-03-09 11:05:57 +0200

    add membership plan and card 1.2 modifications placeholder

--- ubiquity/versioning/v1_2/serializers.py
+++ ubiquity/versioning/v1_2/serializers.py
@@ -1,18 +1,37 @@
+from typing import TYPE_CHECKING
+
 from ubiquity.versioning.base import serializers as base_serializers
 
-MembershipPlanSerializer = base_serializers.MembershipPlanSerializer
-MembershipCardSerializer = base_serializers.MembershipCardSerializer
+if TYPE_CHECKING:
+    from scheme.models import Scheme, SchemeAccount
+
 PaymentCardSerializer = base_serializers.PaymentCardSerializer
 TransactionsSerializer = base_serializers.TransactionsSerializer
 
 
-# TODO: this is just to test if the version switching works, to be removed
-class ServiceConsentSerializer(base_serializers.ServiceConsentSerializer):
-    def to_representation(self, instance):
-        response = {'email': instance.user.email, 'timestamp': int(instance.timestamp.timestamp())}
-        if self._is_valid(instance.latitude) and self._is_valid(instance.longitude):
-            response.update({'latitude': instance.latitude, 'longitude': instance.longitude})
-        return {
-            'consent': response,
-            'version': '1.2'
-        }
+class MembershipPlanSerializer(base_serializers.MembershipPlanSerializer):
+
+    def to_representation(self, instance: 'Scheme') -> dict:
+        plan = super().to_representation(instance)
+
+        # TODO: modify this to return actual fees once they have been implemented
+        plan['fees'] = [{"amount": 1.1, "type": "enrolment"}]
+
+        # TODO: modify this to return actual dynamic content once it has been implemented
+        plan['content'] = [{"column": "content column", "value": "content value"}]
+
+        return plan
+
+
+class MembershipCardSerializer(base_serializers.MembershipCardSerializer):
+    def to_representation(self, instance: 'SchemeAccount') -> dict:
+        card = super().to_representation(instance)
+
+        # TODO: modify this to return actual fields once they have been implemented
+        if 'vouchers' in card:
+            card['vouchers'].update({
+                "body_text": "example body text",
+                "terms_and_conditions_url": "https://www.bink.com"
+            })
+
+        return card
