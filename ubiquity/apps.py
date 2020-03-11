fd26339c2c5a8ebe5a7059d97ec03f925fcebbcd
kaziz <kaziz@bink.com>
2020-03-11 17:10:19 +0000

    Remove commented code

--- ubiquity/apps.py
+++ ubiquity/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 
-from hermes.channel_vault import load_bundle_secrets
-
 
 class UbiquityConfig(AppConfig):
     name = 'ubiquity'
-
-    # def ready(self):
-    #     load_bundle_secrets()
+    verbose_name = 'Ubiquity'
