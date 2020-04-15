77d3f5db76c9fd9a87bb3876d496b4e47b132780
mmarsh <mmarsh@bink.com>
2020-04-15 20:04:29 +0100

    test cases updated

--- ubiquity/tests/test_ubiquity.py
+++ ubiquity/tests/test_ubiquity.py
@@ -1246,7 +1246,7 @@ class TestResources(APITestCase):
         resp = self.client.get(reverse('membership-plan', args=[self.scheme.id]), **self.auth_headers)
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(MockApiCache.available_called)
-        self.assertEqual(MockApiCache.key, 'm_plans:1:test.auth.fake:0:1.2')
+        self.assertEqual(MockApiCache.key, f'm_plans:{self.scheme.id}:test.auth.fake:0:1.2')
         self.assertEqual(MockApiCache.expire, 60 * 60 * 24)
         self.assertDictEqual(MockApiCache.data, resp.json())
 
