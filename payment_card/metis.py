2db0fc84521eca30bd1322d3f975f89b588ee94a
Francesco Milani <fmilani@bink.com>
2020-05-01 14:03:27 +0300

    add sync run option for metis and type checking

--- payment_card/metis.py
+++ payment_card/metis.py
@@ -1,11 +1,19 @@
+from typing import TYPE_CHECKING
+
 import arrow
+import sentry_sdk
+from django.conf import settings
+from requests import request, HTTPError
 
 from payment_card.enums import RequestMethod
 from payment_card.models import PaymentCard
 from payment_card.tasks import async_metis_request
 
+if TYPE_CHECKING:
+    from payment_card.models import PaymentCardAccount
 
-def _generate_card_json(account):
+
+def _generate_card_json(account: 'PaymentCardAccount') -> dict:
     return {
         'payment_token': account.psp_token,
         'card_token': account.token,
@@ -15,36 +23,65 @@ def _generate_card_json(account):
     }
 
 
-def enrol_new_payment_card(account):
-    async_metis_request.delay(
+def enrol_new_payment_card(account: 'PaymentCardAccount', run_async: bool = True) -> None:
+    args = (
         RequestMethod.POST,
         '/payment_service/payment_card',
         _generate_card_json(account)
     )
 
+    if run_async:
+        async_metis_request.delay(*args)
+    else:
+        metis_request(*args)
 
-def update_payment_card(account):
-    async_metis_request.delay(
+
+def update_payment_card(account: 'PaymentCardAccount', run_async: bool = True) -> None:
+    args = (
         RequestMethod.POST,
         '/payment_service/payment_card/update',
         _generate_card_json(account)
     )
+    if run_async:
+        async_metis_request.delay(*args)
+    else:
+        metis_request(*args)
 
 
-def delete_payment_card(account):
-    async_metis_request.delay(
+def delete_payment_card(account: 'PaymentCardAccount', run_async: bool = True) -> None:
+    args = (
         RequestMethod.DELETE,
         '/payment_service/payment_card',
         _generate_card_json(account)
     )
+    if run_async:
+        async_metis_request.delay(*args)
+    else:
+        metis_request(*args)
 
 
-def enrol_existing_payment_card(account):
+def enrol_existing_payment_card(account: 'PaymentCardAccount', run_async: bool = True) -> None:
     provider = account.payment_card.system
 
     if provider in [PaymentCard.VISA, PaymentCard.AMEX]:
-        enrol_new_payment_card(account)
+        enrol_new_payment_card(account, run_async)
     elif provider == PaymentCard.MASTERCARD:
-        update_payment_card(account)
+        update_payment_card(account, run_async)
     else:
         raise ValueError(f"Provider {provider} not found to enrol existing card")
+
+
+def metis_request(method: RequestMethod, endpoint: str, payload: dict) -> None:
+    response = request(
+        method.value,
+        settings.METIS_URL + endpoint,
+        json=payload,
+        headers={
+            'Authorization': 'Token {}'.format(settings.SERVICE_API_KEY),
+            'Content-Type': 'application/json'
+        }
+    )
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        sentry_sdk.capture_exception()
---
+++ payment_card/tests/test_metis_client.py
@@ -0,0 +1,17 @@
+import httpretty
+from django.conf import settings
+from django.test import TestCase
+
+from payment_card.metis import enrol_existing_payment_card
+from payment_card.models import PaymentCard
+from payment_card.tests.factories import PaymentCardAccountFactory
+
+
+class TestMetisClient(TestCase):
+    @httpretty.activate
+    def test_enrol_existing_payment_card(self):
+        httpretty.register_uri(httpretty.POST, settings.METIS_URL + '/payment_service/payment_card/update', status=204)
+        account = PaymentCardAccountFactory()
+        account.payment_card.name = PaymentCard.MASTERCARD
+        enrol_existing_payment_card(account, run_async=False)
+        self.assertTrue(httpretty.has_request())
