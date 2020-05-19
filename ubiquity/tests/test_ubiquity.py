a397672ff078efe78dcb76d450ae941d84211daa
Francesco Milani <fmilani@bink.com>
2020-05-19 14:32:24 +0300

    adapt test

--- ubiquity/tests/test_ubiquity.py
+++ ubiquity/tests/test_ubiquity.py
@@ -981,6 +981,9 @@ class TestResources(APITestCase):
         link = PaymentCardSchemeEntry.objects.filter(pk=entry.pk)
         self.assertEqual(len(link), 0)
 
+    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+                       CELERY_TASK_ALWAYS_EAGER=True,
+                       BROKER_BACKEND='memory')
     def test_payment_card_delete_does_not_delete_link_for_cards_shared_between_users(self):
         external_id = 'test2@user.com'
         user_2 = UserFactory(external_id=external_id, client=self.client_app, email=external_id)
