617a0d99eed1b0572096c814da7552ad851f36a5
mmarsh <mmarsh@bink.com>
2020-05-28 08:25:05 +0100

    Code and test cases updated to use active links

--- payment_card/tests/test_payment_card_user_info.py
+++ payment_card/tests/test_payment_card_user_info.py
@@ -7,6 +7,7 @@ from payment_card.tests import factories as payment_card_factories
 from scheme.tests import factories as scheme_factories
 from scheme.models import SchemeCredentialQuestion
 from user.tests import factories as user_factories
+from ubiquity.models import PaymentCardSchemeEntry
 
 
 class TestPaymentCardUserInfo(APITestCase):
@@ -50,6 +51,69 @@ class TestPaymentCardUserInfo(APITestCase):
                                                                              question=cls.scheme_question)
         cls.scheme_answer_3 = scheme_factories.SchemeCredentialAnswerFactory(scheme_account=cls.scheme_account_3,
                                                                              question=cls.scheme_question)
+
+        cls.link1_1 = PaymentCardSchemeEntry(
+            payment_card_account=cls.payment_card_account_1,
+            scheme_account=cls.scheme_account_1,
+            active_link=True
+        )
+        cls.link1_1.save()
+
+        cls.link1_2 = PaymentCardSchemeEntry(
+            payment_card_account=cls.payment_card_account_1,
+            scheme_account=cls.scheme_account_2,
+            active_link=True
+        )
+        cls.link1_2.save()
+
+        cls.link1_3 = PaymentCardSchemeEntry(
+            payment_card_account=cls.payment_card_account_1,
+            scheme_account=cls.scheme_account_3,
+            active_link=True
+        )
+        cls.link1_3.save()
+
+        cls.link2_1 = PaymentCardSchemeEntry(
+            payment_card_account=cls.payment_card_account_2,
+            scheme_account=cls.scheme_account_1,
+            active_link=True
+        )
+        cls.link2_1.save()
+
+        cls.link2_2 = PaymentCardSchemeEntry(
+            payment_card_account=cls.payment_card_account_2,
+            scheme_account=cls.scheme_account_2,
+            active_link=True
+        )
+        cls.link2_2.save()
+
+        cls.link2_3 = PaymentCardSchemeEntry(
+            payment_card_account=cls.payment_card_account_2,
+            scheme_account=cls.scheme_account_3,
+            active_link=True
+        )
+        cls.link2_3.save()
+
+        cls.link3_1 = PaymentCardSchemeEntry(
+            payment_card_account=cls.payment_card_account_3,
+            scheme_account=cls.scheme_account_1,
+            active_link=True
+        )
+        cls.link3_1.save()
+
+        cls.link3_2 = PaymentCardSchemeEntry(
+            payment_card_account=cls.payment_card_account_3,
+            scheme_account=cls.scheme_account_2,
+            active_link=True
+        )
+        cls.link3_2.save()
+
+        cls.link3_3 = PaymentCardSchemeEntry(
+            payment_card_account=cls.payment_card_account_3,
+            scheme_account=cls.scheme_account_3,
+            active_link=True
+        )
+        cls.link3_3.save()
 
         cls.auth_headers = {'HTTP_AUTHORIZATION': 'Token ' + settings.SERVICE_API_KEY}
         super(TestPaymentCardUserInfo, cls).setUpClass()
@@ -99,7 +163,6 @@ class TestPaymentCardUserInfo(APITestCase):
                                     **self.auth_headers)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content.decode('utf-8'))
-
         self.assertIn('1144**33', data)
         self.assertEqual(data['1144**33']['user_id'], self.user_1.id)
         self.assertEqual(data['1144**33']['scheme_account_id'], self.scheme_account_1.id)
