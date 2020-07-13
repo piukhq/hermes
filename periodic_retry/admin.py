aa7fb612b8cda0fd9574ab07c5deb6b2394815a7
rahimakarbary <rkarbary@bink.com>
2020-07-13 11:40:33 +0200

    Dev done but not tested

--- periodic_retry/admin.py
+++ periodic_retry/admin.py
@@ -1,7 +1,46 @@
 from django.contrib import admin
 
 from .models import PeriodicRetry
+from payment_card.models import PaymentCard, PaymentCardAccount
+from periodic_retry.models import PeriodicRetryStatus,RetryTaskList
+from periodic_retry.tasks import PeriodicRetryHandler
 
+
+def add_visa_active_user(modeladmin, request, queryset):
+    # queryset.update(status='1')
+    payment_card_account = PaymentCardAccount.objects.filter(status=1, payment_card__slug='visa')
+    for visa_card in payment_card_account:
+        PeriodicRetryHandler(task_list=RetryTaskList.METIS_REQUESTS).new(
+            'payment_card.metis', "retry_enrol",
+            context={"card_id": visa_card.id},
+            retry_kwargs={"max_retry_attempts": 1,
+                          "results": [{"caused_by": "migration script"}],
+                          "status": PeriodicRetryStatus.PENDING
+                          }
+        )
+
+
+add_visa_active_user.short_description = "Add VISA Active User"
+
+
+def enrol_visa_user(modeladmin, request, queryset):
+    updated = queryset.update(status=0)
+
+
+    # updated = queryset.update(status=0)
+    # payment_card_account = PaymentCardAccount.objects.filter(status=1, payment_card__slug='visa')
+    # for visa_card in payment_card_account:
+    #     PeriodicRetryHandler(task_list=RetryTaskList.METIS_REQUESTS).new(
+    #         'payment_card.metis', "retry_enrol",
+    #         context={"card_id": visa_card.id},
+    #         retry_kwargs={"max_retry_attempts": 1,
+    #                       "results": [{"caused_by": "migration script"}],
+    #                       "status": PeriodicRetryStatus.REQUIRED
+    #                      }
+    #    )
+
+
+enrol_visa_user.short_description = "Trigger Enrolment"
 
 @admin.register(PeriodicRetry)
 class PeriodicRetryAdmin(admin.ModelAdmin):
@@ -9,3 +48,4 @@ class PeriodicRetryAdmin(admin.ModelAdmin):
                     'created_on',)
     readonly_fields = ('created_on', 'modified_on',)
     search_fields = ('id', 'status', 'task_group', 'function', 'module',)
+    actions = [add_visa_active_user, enrol_visa_user,]
