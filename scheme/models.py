7e904a584a0efc0c60f97c0dd2addb12c717a1ea
kaziz <kaziz@bink.com>
2020-10-05 14:45:09 +0100

    Migrate scheme account transactions to default to list

--- scheme/models.py
+++ scheme/models.py
@@ -641,7 +641,7 @@ class SchemeAccount(models.Model):
     vouchers = JSONField(default=dict, null=True, blank=True)
     card_number = models.CharField(max_length=250, blank=True, db_index=True, default='')
     barcode = models.CharField(max_length=250, blank=True, db_index=True, default='')
-    transactions = JSONField(default=dict, null=True, blank=True)
+    transactions = JSONField(default=list, null=True, blank=True)
     main_answer = models.CharField(max_length=250, blank=True, db_index=True, default='')
     pll_links = JSONField(default=list, null=True, blank=True)
     formatted_images = JSONField(default=dict, null=True, blank=True)
