0b5955ad5603e364a6b88764f67221d4596d4736
Francesco Milani <fmilani@bink.com>
2021-01-18 12:55:58 +0200

    add basic django admin config for historical tables

--- history/admin.py
+++ history/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from history.models import (
+    HistoricalPaymentCardAccount,
+    HistoricalSchemeAccount,
+    HistoricalPaymentCardAccountEntry,
+    HistoricalSchemeAccountEntry,
+)
+
+
+# TODO might be a good idea to capture and add things like Scheme and PaymentCard to the HistoricalTables
+# TODO might be better to add a custom name to the Entry tables to the instance id to avoid confusion
+
+@admin.register(HistoricalPaymentCardAccount)
+class HistoricalPaymentCardAccountAdmin(admin.ModelAdmin):
+    list_display = ("id", "instance_id", "user_id", "created", "change_reason")
+    search_fields = ("instance_id", "user_id", "created")
+    list_filter = ("channel", "change_type")
+
+
+@admin.register(HistoricalPaymentCardAccountEntry)
+class HistoricalPaymentCardAccountEntryAdmin(admin.ModelAdmin):
+    list_display = ("id", "instance_id", "user_id", "payment_card_account_id", "created", "change_reason")
+    search_fields = ("instance_id", "user_id", "payment_card_account_id", "created")
+    list_filter = ("channel", "change_type")
+
+
+@admin.register(HistoricalSchemeAccount)
+class HistoricalSchemeAccountAdmin(admin.ModelAdmin):
+    list_display = ("id", "instance_id", "user_id", "created", "change_reason")
+    search_fields = ("instance_id", "user_id", "created")
+    list_filter = ("channel", "change_type")
+
+
+@admin.register(HistoricalSchemeAccountEntry)
+class HistoricalSchemeAccountEntryAdmin(admin.ModelAdmin):
+    list_display = ("id", "instance_id", "user_id", "scheme_account_id", "created", "change_reason")
+    search_fields = ("instance_id", "user_id", "scheme_account_id", "created")
+    list_filter = ("channel", "change_type")
