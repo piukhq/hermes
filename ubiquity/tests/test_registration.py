62c03a1cf66161bf52fd388c09e19fe9e89bf132
kaziz <kaziz@bink.com>
2021-04-14 17:04:08 +0100

    Add assert to check if consent key is in response test_service_registration_existing_user

--- ubiquity/tests/test_registration.py
+++ ubiquity/tests/test_registration.py
@@ -83,6 +83,7 @@ class TestRegistration(GlobalMockAPITestCase):
         })
         resp = self.client.post('/ubiquity/service', data=consent, content_type='application/json', **auth_headers)
         self.assertEqual(resp.status_code, 200)
+        self.assertIn("consent", resp.data.keys())
 
     def test_service_registration_with_malformed_data_existing_user(self):
         BINK_CLIENT_ID = 'MKd3FfDGBi1CIUQwtahmPap64lneCa2R6GvVWKg6dNg4w9Jnpd'
