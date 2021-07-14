3be7160b5251f53dce97c7bdd8c56df23e0bcc95
simontunmore <stunmore@bink.com>
2021-07-14 11:39:46 +0100

    moved str-dict parsing for message to run.py

--- api_messaging/route.py
+++ api_messaging/route.py
@@ -8,8 +8,6 @@ logger = logging.getLogger(__name__)
 def route_message(headers: dict, message: dict):
     # Route message to core functionality. Route found in headers
 
-    message = ast.literal_eval(message)
-
     route = {
         "add_payment_card": api2_background.add_payment_card,
         "delete_payment_account": api2_background.delete_payment_account,
--- api_messaging/run.py
+++ api_messaging/run.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import django
+import ast
 
 from api_messaging.message_broker import ReceivingService
 from api_messaging.route import route_message
@@ -21,8 +22,9 @@ django.setup(set_prefix=False)
 def on_message_recieved(body, message):
     logger.info("API 2 message received")
     # print(f"got message: {message}  body: {body} headers: {message.headers}")
+    # body is read as str from message - ast.literal eval converts back into dict
     try:
-        success = route_message(message.headers, body)
+        success = route_message(message.headers, ast.literal_eval(body))
     except RuntimeError:
         if not message.acknowledged:
             message.reject()
