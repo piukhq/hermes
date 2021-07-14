f06bd8a85fb74205a354528ea2daa0c06051711a
Tony Truong <ttruong@bink.com>
2021-07-14 11:45:08 +0100

    Exclude broker from coverage

---
+++ api_messaging/management/commands/run_message_receiver.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from api_messaging.message_broker import ReceivingService
+from api_messaging.run import on_message_recieved, on_time_out
+
+
+def run_receiver():
+    ReceivingService(
+        user=settings.RABBIT_USER,
+        password=settings.RABBIT_PASSWORD,
+        host=settings.RABBIT_HOST,
+        port=settings.RABBIT_PORT,
+        queue_name="from_api2",
+        heartbeat=settings.TIME_OUT * 3,
+        timeout=settings.TIME_OUT,
+        callbacks=[on_message_recieved],
+        on_time_out=on_time_out
+    )
+
+
+class Command(BaseCommand):
+    help = "Run receiver to process messages from Angelia."
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Running receiver service.')
+        run_receiver()
