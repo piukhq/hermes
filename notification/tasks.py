2e39b61649c9080f545607133d877b57a740a124
Tony Truong <ttruong@bink.com>
2021-07-21 18:04:09 +0100

    Added tests

--- notification/tasks.py
+++ notification/tasks.py
@@ -3,16 +3,21 @@ import logging
 from datetime import timedelta
 from time import time, sleep
 
+import pysftp
 from celery import shared_task
 from django.conf import settings
 from django.utils import timezone
+from paramiko import SSHException
 from pysftp import Connection, ConnectionException
 
 from history.models import HistoricalSchemeAccount
+from scheme.models import SchemeAccount
 from ubiquity.models import SchemeAccountEntry
 
 
 logger = logging.getLogger(__name__)
+
+STATUS_MAP = {x[0]: x[1] for x in SchemeAccount.EXTENDED_STATUSES}
 
 
 class SftpManager:
@@ -21,10 +26,11 @@ class SftpManager:
         self.sftp_username = settings.SFTP_USERNAME
         self.sftp_password = settings.SFTP_PASSWORD
         self.rows = rows
+        self.cnopts = pysftp.CnOpts()
 
     @staticmethod
     def format_data(data):
-        return [["01", x[0], x[1], x[2], x[3]] for x in data]
+        return [['01', x[0], x[1], STATUS_MAP[x[2]], x[3].timestamp()] for x in data]
 
     def transfer_file(self):
         date = timezone.now().strftime('%Y%m%d')
@@ -33,26 +39,30 @@ class SftpManager:
         rows = self.format_data(self.rows)
 
         errors = 0
-        run = True
 
-        while run:
+        while True:
             try:
-                with Connection(self.host, username=self.sftp_username, password=self.sftp_password) as sftp:
-                    logging.info("Successfully connected to SFTP.")
+                with Connection(
+                        self.host,
+                        username=self.sftp_username,
+                        password=self.sftp_password,
+                        cnopts=self.cnopts
+                ) as sftp:
+                    logging.info('Successfully connected to SFTP.')
                     with sftp.open(filename, 'w', bufsize=32768) as f:
                         writer = csv.writer(f)
                         writer.writerow([00, date])
                         writer.writerows(self.rows)
                         writer.writerow([99, len(rows)])
-                        run = False
-            except ConnectionException as e:
-                logging.info("Retrying notification file in 2 minutes.")
-                sleep(120)
-                if errors == 5:
-                    logging.warning(f"Failed to transfer file. Error - {e}")
-                    run = False
-
-        logging.info(f"File: {filename}, uploaded.")
+                    logging.info(f'File: {filename}, uploaded.')
+                    return
+            except (ConnectionException, SSHException) as e:
+                errors += 1
+                logging.info('Retrying notification file in 2 minutes.')
+                sleep(settings.NOTIFICATION_RETRY_TIMER)
+                if errors == settings.NOTIFICATION_ERROR_THRESHOLD:
+                    logging.warning(f'Failed to transfer file. Error - {e}')
+                    raise e
 
 
 class NotificationProcessor:
@@ -61,14 +71,15 @@ class NotificationProcessor:
         self.to_date = to_date
 
     def get_data(self):
-        change_type = "status"
+        rows_to_write = []
+        change_type = 'status'
         scheme_accounts_entries = SchemeAccountEntry.objects.filter(
             user__client__organisation__name=self.org
         )
 
         # initiation file data
         if not self.to_date:
-            rows = scheme_accounts_entries.values_list(
+            rows_to_write = scheme_accounts_entries.values_list(
                 'user__external_id',
                 'scheme_account__scheme__name',
                 'scheme_account__status',
@@ -78,21 +89,41 @@ class NotificationProcessor:
             # Zero out provided time
             to_datetime = self.to_date.replace(microsecond=0, second=0, minute=0)
 
-            # Get data for the last 2 hours
+            # Get any status changes in the last 2 hours where status has changed
             from_datetime = to_datetime - timedelta(hours=2)
-            list_of_ids = scheme_accounts_entries.values_list('scheme_account_id')
-            rows = HistoricalSchemeAccount.objects.filter(
+            list_of_ids = list(scheme_accounts_entries.values_list('scheme_account_id', flat=True))
+            historical_rows = list(HistoricalSchemeAccount.objects.filter(
                 instance_id__in=list_of_ids,
-                change_details=change_type,
+                change_details__contains=change_type,
                 created__range=[from_datetime, to_datetime]
-            ).values_list(
-                'user__external_id',
-                'scheme_account__scheme__name',
-                'scheme_account__status',
-                'scheme_account__created',
-            )
+            ).values('instance_id', 'body', 'created'))
 
-        return rows
+            # Need the values from SchemeAccount and the created date from HistoricalSchemeAccount
+            if historical_rows:
+                ids_to_filter = [row["instance_id"] for row in historical_rows]
+                rows = scheme_accounts_entries.filter(scheme_account_id__in=ids_to_filter).values(
+                    'scheme_account_id',
+                    'user__external_id',
+                    'scheme_account__scheme__name',
+                    'scheme_account__status',
+                )
+
+                for row in rows:
+                    for counter, value in enumerate(historical_rows):
+                        if int(value['instance_id']) == row['scheme_account_id']:
+                            rows_to_write.append([
+                                row['user__external_id'],
+                                row['scheme_account__scheme__name'],
+                                value['body']['status'],
+                                value['created']
+                            ])
+
+                            # Remove from list so we don't have to loop through it again
+                            historical_rows.pop(counter)
+
+                            break
+
+        return rows_to_write
 
 
 @shared_task()
--- notification/tests/test_notification_tasks.py
+++ notification/tests/test_notification_tasks.py
@@ -1,20 +1,117 @@
-from unittest.mock import patch
+from datetime import timedelta
+from unittest import mock
+
+import pysftp
+from django.conf import settings
+from django.utils import timezone
+from paramiko import SSHException
 
 from history.models import HistoricalSchemeAccount
 from history.utils import GlobalMockAPITestCase
-from notification.tasks import SftpManager, NotificationProcessor
-from ubiquity.tests.factories import SchemeAccountFactory
+from notification.tasks import SftpManager, NotificationProcessor, STATUS_MAP
+from scheme.models import SchemeAccount
+from scheme.tests.factories import SchemeAccountFactory
+from ubiquity.tests.factories import SchemeAccountEntryFactory
+from user.tests.factories import UserFactory, ClientApplicationFactory
 
 
 class TestNotificationTask(GlobalMockAPITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.test_org = "Barclays"
-        cls.scheme_account = SchemeAccountFactory(user__client__organisation__name=cls.test_org)
+        cls.test_org = "Barlcays"
+        cls.external_id = "Test_User"
+        cls.client_application = ClientApplicationFactory(organisation__name=cls.test_org)
+        cls.user = UserFactory(client=cls.client_application, external_id=cls.external_id)
+        cls.scheme_account = SchemeAccountFactory(status=SchemeAccount.PENDING)
+        cls.scheme_account_entry = SchemeAccountEntryFactory(
+            user=cls.user, scheme_account=cls.scheme_account
+        )
+
+        cls.history_scheme_account = HistoricalSchemeAccount(
+            change_type=HistoricalSchemeAccount.CREATE,
+            instance_id=cls.scheme_account.id,
+            change_details='status',
+            body={"id": cls.scheme_account.id}
+        ).save()
+
+        settings.NOTIFICATION_ERROR_THRESHOLD = 1
+        settings.NOTIFICATION_RETRY_TIMER = 1
 
     def test_get_data_initiation(self):
         test_notification = NotificationProcessor(organisation=self.test_org)
         data = test_notification.get_data()
+        expected_result = (
+            self.scheme_account_entry.user.external_id,
+            self.scheme_account_entry.scheme_account.scheme.name,
+            self.scheme_account_entry.scheme_account.status,
+            self.scheme_account_entry.scheme_account.created
+        )
 
-        self.assertEqual(data[0][0], self.scheme_account.id)
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0][0], self.scheme_account_entry.user.external_id)
+        self.assertEqual(data[0], expected_result)
+
+    def test_get_historical_data(self):
+        mocked_datetime = timezone.now() + timedelta(hours=2)
+        with mock.patch('django.utils.timezone.now', mock.Mock(return_value=mocked_datetime)):
+            HistoricalSchemeAccount(
+                change_type=HistoricalSchemeAccount.UPDATE,
+                instance_id=self.scheme_account_entry.scheme_account.id,
+                change_details='status',
+                body={"id": self.scheme_account.id, "status": SchemeAccount.ACTIVE}
+            ).save()
+
+        historical_scheme_accounts = HistoricalSchemeAccount.objects.all()
+        self.assertEqual(len(historical_scheme_accounts), 2)
+        self.assertEqual(historical_scheme_accounts[1].created, mocked_datetime)
+
+        three_hours_plus = timezone.now() + timedelta(hours=3)
+        with mock.patch('django.utils.timezone.now', mock.Mock(return_value=three_hours_plus)):
+            test_notification = NotificationProcessor(
+                organisation=self.test_org, to_date=timezone.now()
+            )
+            data = test_notification.get_data()
+
+            self.assertEqual(len(data), 1)
+            self.assertEqual(data[0][2], SchemeAccount.ACTIVE)
+
+    def test_data_format(self):
+        datetime_now = timezone.now()
+
+        data = [[self.external_id, self.scheme_account.scheme.name, SchemeAccount.ACTIVE, datetime_now]]
+        expected_result = [
+            [
+                '01',
+                self.external_id,
+                self.scheme_account.scheme.name,
+                STATUS_MAP[SchemeAccount.ACTIVE],
+                datetime_now.timestamp()
+            ]
+        ]
+
+        sftp = SftpManager()
+        result = sftp.format_data(data)
+
+        self.assertEqual(result, expected_result)
+
+    def test_retry_raise_exception(self):
+        sftp = SftpManager(rows=[])
+        with self.assertRaises(SSHException):
+            sftp.transfer_file()
+
+    @mock.patch('pysftp.Connection')
+    @mock.patch('notification.tasks.SftpManager.transfer_file')
+    def test_transfer_file(self, mock_connection, mock_transfer):
+        # Disable host key checking for tests
+        test_cnopts = pysftp.CnOpts()
+        test_cnopts.hostkeys = None
+
+        test_data = [
+            [self.external_id, self.scheme_account.scheme.name, SchemeAccount.ACTIVE, timezone.now()]
+        ]
+        sftp = SftpManager(rows=test_data)
+        sftp.cnopts = test_cnopts
+        result = sftp.transfer_file()
+
+        self.assertTrue(result)
