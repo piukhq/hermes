f863c2cc97c21a8198e900f9b5d1e086e7ae0513
simontunmore <stunmore@bink.com>
2021-07-22 15:08:25 +0100

    changed add_card journey from angelia to run sync

--- api_messaging/angelia_background.py
+++ api_messaging/angelia_background.py
@@ -23,7 +23,7 @@ def post_payment_account(message: dict):
         )
 
     if message.get("created"):
-        metis.enrol_new_payment_card(payment_card_account)
+        metis.enrol_new_payment_card(payment_card_account, run_async=False)
 
 
 def delete_payment_account(message: dict):
