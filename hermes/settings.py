8abd703d5a770e75fe33822ad64d67ca8530f170
kaziz <kaziz@bink.com>
2021-08-12 15:26:15 +0100

    Update celery settings to new lowercase variables and set worker_enable_remote_control to False

--- hermes/settings.py
+++ hermes/settings.py
@@ -457,11 +457,13 @@ INFLUX_DB_CONFIG = {
     "password": env_var("INFLUX_PASSWORD", ""),
 }
 
-CELERY_BROKER_URL = env_var("CELERY_BROKER_URL", "pyamqp://guest@localhost//")
-CELERY_TASK_DEFAULT_QUEUE = env_var("CELERY_TASK_DEFAULT_QUEUE", "ubiquity-async-midas")
-CELERY_TASK_SERIALIZER = "pickle"
-CELERY_ACCEPT_CONTENT = ["pickle", "json"]
-CELERY_RESULT_SERIALIZER = "pickle"
+# Celery
+broker_url = env_var("CELERY_BROKER_URL", "pyamqp://guest@localhost//")
+task_default_queue = env_var("CELERY_TASK_DEFAULT_QUEUE", "ubiquity-async-midas")
+task_serializer = "pickle"
+accept_content = ["pickle", "json"]
+result_serializer = "pickle"
+worker_enable_remote_control = False
 
 SPREEDLY_BASE_URL = env_var("SPREEDLY_BASE_URL", "")
 
