607d1a540f9d4a8f1f1699b0b272cf34d850be6d
Tony Truong <ttruong@bink.com>
2021-09-08 15:41:45 +0100

    Typo fix and line breaks

--- notification/stfp_connect.py
+++ notification/stfp_connect.py
@@ -20,28 +20,36 @@ class HostKey(NamedTuple):
             pass
 
 
-def password_and_key_sftp_clien(host, port, host_keys, username, password, pkey):
+def password_and_key_sftp_client(host, port, host_keys, username, password, pkey):
     host_key_collection = paramiko.HostKeys()
     server_hostkey_name = "[{}]:{}".format(host, port) if port != 22 else host
+
     for host_key in host_keys:
         host_key_collection.add(host_key.host, host_key.keytype, host_key.parsed_key)
+
     transport = paramiko.Transport((host, port))
     transport.start_client(event=None, timeout=15)
     remote_key = transport.get_remote_server_key()
+
     if not host_key_collection.check(server_hostkey_name, remote_key):
         raise paramiko.BadHostKeyException(server_hostkey_name, remote_key, host_keys[0].parsed_key)
+
     transport.auth_publickey(username, pkey, event=None)
+
     # Setup an auth handler
     pw_auth_event = threading.Event()
     pw_auth_handler = paramiko.auth_handler.AuthHandler(transport)
     transport.auth_handler = pw_auth_handler
+
     # Get transport lock so other threads cant sent stuff
     transport.lock.acquire()
-    # Assenble auth event
+
+    # Assemble auth event
     pw_auth_handler.auth_event = pw_auth_event
     pw_auth_handler.auth_method = 'password'
     pw_auth_handler.username = username
     pw_auth_handler.password = password
+
     # Sent auth message
     userauth_message = paramiko.message.Message()
     userauth_message.add_string('ssh-userauth')
@@ -49,6 +57,7 @@ def password_and_key_sftp_clien(host, port, host_keys, username, password, pkey)
     pw_auth_handler._parse_service_accept(userauth_message)
     transport.lock.release()
     pw_auth_handler.wait_for_response(pw_auth_event)
+
     if not transport.is_authenticated():
         raise ValueError("Failed to authenticate with multi-factor auth")
     return transport.open_sftp_client()
@@ -72,6 +81,6 @@ def get_sftp_client(
             ssh_client.connect(hostname=host, port=port, username=username, password=password)
         return ssh_client.open_sftp()
     elif pkey and password:
-        return password_and_key_sftp_clien(host, port, host_keys, username, password, pkey)
+        return password_and_key_sftp_client(host, port, host_keys, username, password, pkey)
 
     raise ValueError("password and or private key must be provided")
