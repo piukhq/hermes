7841e6d1e8bdfa9fde79f825ea89af22d3ed70e7
simontunmore <stunmore@bink.com>
2021-10-29 11:25:05 +0100

    refactoring

--- ubiquity/views.py
+++ ubiquity/views.py
@@ -801,9 +801,9 @@ class MembershipCardView(
     @censor_and_decorate
     def destroy(self, request, *args, **kwargs):
         scheme_account = self.get_object()
-        if scheme_account.status in (SchemeAccount.JOIN_PENDING, SchemeAccount.REGISTRATION_ASYNC_IN_PROGRESS):
-            # Ideally we would create a different message for pending registrations as this is a little misleading, but
-            # avoiding non-agreed changes to the API for Barclays so will keep this the same for now.
+        if scheme_account.status in (SchemeAccount.JOIN_PENDING + SchemeAccount.REGISTER_PENDING):
+            # Ideally we would create a different error message for pending registrations as this is a little misleading
+            # , but this would mean non-agreed changes to the API for Barclays so will keep this the same for now.
             error = {"join_pending": "Membership card cannot be deleted until the Join process has completed."}
             return Response(error, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
