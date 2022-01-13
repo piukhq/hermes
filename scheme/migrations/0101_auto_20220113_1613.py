f41de728135250a13b78473cfc086138eab9f972
MartfBink <mfranklin@bink.com>
2022-01-13 17:18:16 +0000

    STYLE MARTIN STYLE

--- scheme/migrations/0101_auto_20220113_1613.py
+++ scheme/migrations/0101_auto_20220113_1613.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('scheme', '0100_auto_20211213_1126'),
+        ("scheme", "0100_auto_20211213_1126"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='scheme',
-            name='tier',
-            field=models.IntegerField(choices=[(1, 'PLL'), (2, 'Store'), (3, 'Engage'), (4, 'Coming Soon')]),
+            model_name="scheme",
+            name="tier",
+            field=models.IntegerField(choices=[(1, "PLL"), (2, "Store"), (3, "Engage"), (4, "Coming Soon")]),
         ),
     ]
