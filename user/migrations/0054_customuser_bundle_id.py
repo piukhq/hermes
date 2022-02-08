760d3ee2b7b3ab3c9b304e4f000f92f5275330fc
MartfBink <mfranklin@bink.com>
2022-02-08 15:37:07 +0000

    populate bundle_id from clientapplicationbundle

--- user/migrations/0054_customuser_bundle_id.py
+++ user/migrations/0054_customuser_bundle_id.py
@@ -3,7 +3,25 @@
 from django.db import migrations, models
 
 
+
+def populate_bundle_id(apps, schema_editor):
+    # some users in dev database had no bundle_id
+    # hence the check for cab
+    CustomUser = apps.get_model("user", "CustomUser")
+    for user in CustomUser.all_objects.filter(is_active=True).all():        
+        cab = user.client.clientapplicationbundle_set.first()
+        if cab:
+            user.bundle_id = cab.bundle_id
+            user.save()
+    
+
+def reverse_bundle_id(apps, schema_editor):
+    # once field is removed nothing more to do here
+    pass
+
+
 class Migration(migrations.Migration):
+
 
     dependencies = [
         ("user", "0053_clientapplicationbundle_email_required"),
@@ -15,4 +33,5 @@ class Migration(migrations.Migration):
             name="bundle_id",
             field=models.CharField(blank=True, max_length=255),
         ),
+        migrations.RunPython(populate_bundle_id, reverse_bundle_id),
     ]
