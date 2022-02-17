36cbd810f14ec7a67a11f745d61ba31ee8073ea4
MartfBink <mfranklin@bink.com>
2022-02-17 10:23:15 +0000

    allow the script to do more than 10

--- scripts/find_errors/scheme_accounts_invalid_creds.py
+++ scripts/find_errors/scheme_accounts_invalid_creds.py
@@ -7,7 +7,7 @@ from .base_script import BaseScript
 class FindSchemeAccountsStuckInInvalidCreds(BaseScript):
     def script(self):
         scheme_accounts = SchemeAccount.objects.filter(status=SchemeAccount.INVALID_CREDENTIALS)
-        for scheme_account in scheme_accounts[:10]:  # just do 10 martin
+        for scheme_account in scheme_accounts:
             self.set_correction(SchemeAccountCorrection.MARK_AS_UNKNOWN)
             self.make_correction(str(scheme_account.id), {"schemeaccount_id": scheme_account.id})
             self.result.append(f"schemeaccount_id: {scheme_account.id}" f" script:{self.correction_title}")
