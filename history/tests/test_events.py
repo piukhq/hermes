da287c8062c0dba69fbc7f83d894099a3bc55bd2
mmarsh <mmarsh@bink.com>
2022-04-13 13:47:20 +0100

    includes all sprint loyalty card events tickets and tests include fix for LOY_2351 and implementation of LOY-2411 and 2412

--- history/tests/test_events.py
+++ history/tests/test_events.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 from django.test import TransactionTestCase
 
-from history import data_warehouse
+from history.data_warehouse import join_outcome, remove_loyalty_card_event
 from scheme.models import SchemeBundleAssociation
 from scheme.tests.factories import SchemeAccountFactory, SchemeBundleAssociationFactory, SchemeFactory
 from user.tests.factories import (
@@ -36,7 +36,7 @@ class TestEventHandlers(TransactionTestCase):
 
     @patch("history.data_warehouse.to_data_warehouse")
     def test_remove_loyalty_card_event(self, mock_to_warehouse):
-        data_warehouse.remove_loyalty_card_event(self.user, self.mcard)
+        remove_loyalty_card_event(self.user, self.mcard)
         self.assertTrue(mock_to_warehouse.called)
         data = mock_to_warehouse.call_args.args[0]
         self.assertEqual(data["event_type"], "lc.removed")
@@ -48,6 +48,101 @@ class TestEventHandlers(TransactionTestCase):
         self.assertEqual(data["scheme_account_id"], self.mcard.id)
         self.assertEqual(data["loyalty_plan"], self.mcard.scheme_id)
         self.assertEqual(data["main_answer"], self.mcard.main_answer)
+        self.assertEqual(data["status"], self.mcard.status)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.mcard.delete()
+        cls.scheme.delete()
+        cls.user.delete()
+        cls.bundle.delete()
+        cls.client_app.delete()
+        cls.organisation.delete()
+        super().tearDownClass()
+
+
+class TestJoinSuccessEventHandlers(TransactionTestCase):
+    reset_sequences = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.organisation = OrganisationFactory(name="event_test_organisation")
+        cls.client_app = ClientApplicationFactory(
+            organisation=cls.organisation,
+            name="event test client application",
+            client_id="2zXAKlzMwU5mefvs4NtWrQNDNXYrDdLwWeSCoCCrjd8N0VAbcdef",
+        )
+        cls.bundle = ClientApplicationBundleFactory(bundle_id="test.auth.fake", client=cls.client_app)
+
+        cls.user = UserFactory(external_id="test@delete.user", client=cls.client_app, email="test@delete.user")
+        cls.scheme = SchemeFactory()
+
+        SchemeBundleAssociationFactory(scheme=cls.scheme, bundle=cls.bundle, status=SchemeBundleAssociation.ACTIVE)
+
+        cls.mcard = SchemeAccountFactory(scheme=cls.scheme)
+        super().setUpClass()
+
+    @patch("history.data_warehouse.to_data_warehouse")
+    def test_success_join(self, mock_to_warehouse):
+        join_outcome(True, self.user, self.mcard)
+        self.assertTrue(mock_to_warehouse.called)
+        data = mock_to_warehouse.call_args.args[0]
+        self.assertEqual(data["event_type"], "lc.join.success")
+        self.assertEqual(data["origin"], "merchant.callback")
+        self.assertEqual(data["channel"], "test.auth.fake")
+        self.assertEqual(data["external_user_ref"], self.user.external_id)
+        self.assertEqual(data["internal_user_ref"], self.user.id)
+        self.assertEqual(data["email"], self.user.email)
+        self.assertEqual(data["scheme_account_id"], self.mcard.id)
+        self.assertEqual(data["loyalty_plan"], self.mcard.scheme_id)
+        self.assertEqual(data["main_answer"], self.mcard.main_answer)
+        self.assertEqual(data["status"], self.mcard.status)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.mcard.delete()
+        cls.scheme.delete()
+        cls.user.delete()
+        cls.bundle.delete()
+        cls.client_app.delete()
+        cls.organisation.delete()
+        super().tearDownClass()
+
+
+class TestJoinFailEventHandlers(TransactionTestCase):
+    reset_sequences = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.organisation = OrganisationFactory(name="event_test_organisation")
+        cls.client_app = ClientApplicationFactory(
+            organisation=cls.organisation,
+            name="event test client application",
+            client_id="2zXAKlzMwU5mefvs4NtWrQNDNXYrDdLwWeSCoCCrjd8N0VAbcdef",
+        )
+        cls.bundle = ClientApplicationBundleFactory(bundle_id="test.auth.fake", client=cls.client_app)
+
+        cls.user = UserFactory(external_id="test@delete.user", client=cls.client_app, email="test@delete.user")
+        cls.scheme = SchemeFactory()
+
+        SchemeBundleAssociationFactory(scheme=cls.scheme, bundle=cls.bundle, status=SchemeBundleAssociation.ACTIVE)
+
+        cls.mcard = SchemeAccountFactory(scheme=cls.scheme)
+        super().setUpClass()
+
+    @patch("history.data_warehouse.to_data_warehouse")
+    def test_failed_join(self, mock_to_warehouse):
+        join_outcome(False, self.user, self.mcard)
+        self.assertTrue(mock_to_warehouse.called)
+        data = mock_to_warehouse.call_args.args[0]
+        self.assertEqual(data["event_type"], "lc.join.failed")
+        self.assertEqual(data["origin"], "merchant.callback")
+        self.assertEqual(data["channel"], "test.auth.fake")
+        self.assertEqual(data["external_user_ref"], self.user.external_id)
+        self.assertEqual(data["internal_user_ref"], self.user.id)
+        self.assertEqual(data["email"], self.user.email)
+        self.assertEqual(data["scheme_account_id"], self.mcard.id)
+        self.assertEqual(data["loyalty_plan"], self.mcard.scheme_id)
         self.assertEqual(data["status"], self.mcard.status)
 
     @classmethod
