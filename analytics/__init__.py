292e49a483378edf24b20da04deb069b8d6c8952
simontunmore <stunmore@bink.com>
2022-09-12 10:43:34 +0100

    Fully removed analytics app and folder

--- analytics/__init__.py
+++
@@ -1,5 +0,0 @@
-import analytics.events  # noqa
-from analytics.api import (  # noqa
-    SETTING_CUSTOM_ATTRIBUTES,
-    PushError,
-)
--- analytics/api.py
+++
@@ -1,6 +0,0 @@
-OLYMPUS_SERVICE_TRACKING_TYPE = 6  # Defined in Mnemosyne project
-SETTING_CUSTOM_ATTRIBUTES = ["marketing-bink", "marketing-external"]
-
-
-class PushError(Exception):
-    pass
