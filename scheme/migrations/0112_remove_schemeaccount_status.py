590d45d7ebf807ec970ac15ebb673f5660b855b8
simontunmore <stunmore@bink.com>
2022-09-20 09:20:41 +0100

    rejigged scheme_account_status migration

--- scheme/migrations/0112_remove_schemeaccount_status.py
+++ scheme/migrations/0112_remove_schemeaccount_status.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("scheme", "0109_rename_main_answer_schemeaccount_alt_main_answer"),
+        ("scheme", "0111_rename_main_answer_schemeaccount_alt_main_answer"),
     ]
 
     operations = [
