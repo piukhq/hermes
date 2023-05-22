73d77a88c10654f2bfa29f2b03b6aa704ae90472
Tony Truong <ttruong@bink.com>
2023-05-22 17:46:52 +0100

    select_for_update queryset

--- scripts/actions/ubiquity_actions.py
+++ scripts/actions/ubiquity_actions.py
@@ -1,14 +1,18 @@
+from django.db import transaction
+
 from ubiquity.models import PaymentCardSchemeEntry
 
 
 def do_update_active_link_to_false(entry: dict) -> bool:
     try:
-        payment_scheme_entry = PaymentCardSchemeEntry.objects.select_related().filter(
+        import pdb; pdb.set_trace()
+        payment_scheme_entry = PaymentCardSchemeEntry.objects.select_for_update().filter(
             id=entry.data["paymentcardschemeentry_id"]
         )
 
-        payment_scheme_entry[0].active_link = False
-        payment_scheme_entry[0].save(update_fields=["active_link"])
+        with transaction.atomic():
+            payment_scheme_entry[0].active_link = False
+            payment_scheme_entry[0].save(update_fields=["active_link"])
 
         return True
 
