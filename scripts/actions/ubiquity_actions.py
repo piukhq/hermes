6755c4bdf049ed84a60ca47a0a677c702788df48
Tony Truong <ttruong@bink.com>
2023-05-23 12:06:13 +0100

    Move query within atomic

--- scripts/actions/ubiquity_actions.py
+++ scripts/actions/ubiquity_actions.py
@@ -1,11 +1,17 @@
+from django.db import transaction
+
 from ubiquity.models import PaymentCardSchemeEntry
 
 
 def do_update_active_link_to_false(entry: dict) -> bool:
     try:
-        payment_scheme_entry = PaymentCardSchemeEntry.objects.get(id=entry.data["paymentcardschemeentry_id"])
-        payment_scheme_entry.active_link = False
-        payment_scheme_entry.save(update_fields=["active_link"])
+
+        with transaction.atomic():
+            payment_scheme_entry = PaymentCardSchemeEntry.objects.select_for_update().get(
+                id=entry.data["paymentcardschemeentry_id"]
+            )
+            payment_scheme_entry.active_link = False
+            payment_scheme_entry.save(update_fields=["active_link"])
 
         return True
 
