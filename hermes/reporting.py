5b2407268fc66f85d6e1dedeb1cc5beaa9c9bf9d
Francesco Milani <fmilani@bink.com>
2023-07-17 13:49:47 +0300

    WAL-3079 implement bink-logging-utils library

--- hermes/reporting.py
+++ hermes/reporting.py
@@ -1,49 +1,5 @@
-import json
-import logging
-from typing import TYPE_CHECKING, cast
+from bink_logging_utils.gunicorn import gunicorn_logger_factory
+from bink_logging_utils.handlers import loguru_intercept_handler_factory
 
-from gunicorn.glogging import Logger as GLogger
-
-if TYPE_CHECKING:
-    from gunicorn.config import Config
-
-
-class JSONFormatter(logging.Formatter):
-    def __init__(self) -> None:
-        pass
-
-    def format(self, record: logging.LogRecord) -> str:  # noqa: A003
-        fmt_record = {
-            "timestamp": record.created,
-            "level": record.levelno,
-            "levelname": record.levelname,
-            "process": record.processName,
-            "thread": record.threadName,
-            "file": record.pathname,
-            "line": record.lineno,
-            "module": record.module,
-            "function": record.funcName,
-            "name": record.name,
-        }
-
-        if record.name == "gunicorn.access" and (val := record.args.get("{x-azure-ref}i")):
-            fmt_record["x_azure_ref"] = val
-
-        fmt_record["message"] = record.getMessage()
-        return json.dumps(fmt_record)
-
-
-class GunicornAccessFiltering(logging.Filter):
-    def filter(self, record: logging.LogRecord) -> bool:
-        if request_info := cast(str, record.args.get("r")):
-            return not any(endpoint in request_info for endpoint in ("/livez", "/readyz", "/metrics"))
-
-        return True
-
-
-class CustomGunicornLogger(GLogger):
-    def setup(self, cfg: "Config") -> None:
-        super().setup(cfg)
-        self._set_handler(self.error_log, cfg.errorlog, JSONFormatter())
-        self._set_handler(self.access_log, cfg.accesslog, JSONFormatter())
-        self.access_log.addFilter(GunicornAccessFiltering())
+InterceptHandler = loguru_intercept_handler_factory()
+CustomGunicornLogger = gunicorn_logger_factory(intercept_handler_class=InterceptHandler)
