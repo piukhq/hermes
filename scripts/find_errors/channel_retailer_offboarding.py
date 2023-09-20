d762469ea1f5ca709514284d517b017d124ce466
Francesco Milani <fmilani@bink.com>
2023-09-20 16:49:16 +0300

    WAL-3200 iceland offboarding

--- scripts/find_errors/channel_retailer_offboarding.py
+++ scripts/find_errors/channel_retailer_offboarding.py
@@ -1,8 +1,7 @@
-from scheme.models import SchemeAccount
-from user.models import ClientApplicationBundle
+from django.db import connection
 
-from ..actions.corrections import Correction
-from .base_script import BaseScript
+from scripts.actions.corrections import Correction
+from scripts.find_errors.base_script import BaseScript
 
 
 class _FindRetailerAndChannelMembershipCards(BaseScript):
@@ -18,24 +17,66 @@ class _FindRetailerAndChannelMembershipCards(BaseScript):
     scheme_slug = "N/A"
     channels = []
 
+    def _get_scheme_account_id_and_channels_map(self) -> str:
+        """
+        Example response structure:
+        ```python
+        [
+            (
+                SchemeAccount.id,
+                {
+                    ClientApplication.client_id: {
+                        "id": ClientApplicationBundle.id,
+                        "channel": ClientApplicationBundle.bundle_id
+                    }
+                }
+            ),
+            ...
+        ]
+        ```
+        """
+
+        params = [self.scheme_slug]
+        sql = """
+            SELECT
+                sa.id,
+                jsonb_object_agg(
+                    cab.client_id ,
+                    jsonb_build_object(
+                        'id',
+                        cab.id::TEXT,
+                        'channel',
+                        cab.bundle_id
+                    )
+                )
+            FROM scheme_schemeaccount sa
+            JOIN scheme_scheme s ON sa.scheme_id = s.id
+            JOIN ubiquity_schemeaccountentry sae ON sa.id = sae.scheme_account_id
+            JOIN "user" u ON sae.user_id = u.id
+            JOIN user_clientapplication ca ON u.client_id = ca.client_id
+            JOIN user_clientapplicationbundle cab ON ca.client_id = cab.client_id
+            WHERE s.slug = %s
+            AND sa.is_deleted = false
+        """
+        if self.channels:
+            params.append(tuple(self.channels))
+            sql += "\n AND cab.bundle_id IN %s"
+
+        sql += "\n GROUP BY sa.id;"
+
+        with connection.cursor() as cursor:
+            cursor.execute(sql, params)
+            res = cursor.fetchall()
+
+        return res
+
     def script(self):
-        clients_to_bundle_map = dict(
-            ClientApplicationBundle.objects.filter(bundle_id__in=self.channels).values_list(
-                "client", "bundle_id", named=False
-            )
-        )
-
-        scheme_accounts = SchemeAccount.objects.values_list("id", flat=True).filter(
-            scheme__slug__iexact=self.scheme_slug,
-            user_set__client_id__in=clients_to_bundle_map.keys(),
-            is_deleted=False,
-        )
-
-        for scheme_account_id in scheme_accounts:
+        for scheme_account_id, client_channel_map in self._get_scheme_account_id_and_channels_map():
             self.set_correction(Correction.CHANNELS_RETAILER_OFFBOARDING)
+            channels_ids = [v["id"] for v in client_channel_map.values()]
             self.make_correction(
-                unique_id_string=f"{str(scheme_account_id)}.{self.scheme_slug}.{'.'.join(self.channels)}",
-                data={"scheme_account_id": scheme_account_id, "clients_to_bundle_map": clients_to_bundle_map},
+                unique_id_string=(f"{str(scheme_account_id)}.{self.scheme_slug}.[{','.join(channels_ids)}]"),
+                data={"scheme_account_id": scheme_account_id, "client_channel_map": client_channel_map},
             )
             self.result.append(f"scheme_account_id: {scheme_account_id} " f"script:{self.correction_title}")
 
@@ -48,3 +89,7 @@ class FindBarclaysBinkWasabiMembershipCards(_FindRetailerAndChannelMembershipCar
 class FindBarclaysViatorMembershipCards(_FindRetailerAndChannelMembershipCards):
     scheme_slug = "bpl-viator"
     channels = ["com.barclays.bmb"]
+
+
+class FindAllChannelsIcelandMembershipCards(_FindRetailerAndChannelMembershipCards):
+    scheme_slug = "iceland-bonus-card"
