9acff758b91e6a9f313c047a4c89caaf44aefbfb
Francesco Milani <fmilani@bink.com>
2024-04-18 12:17:55 +0300

    exclude migrations from ruff linter but not from formatter

--- scripts/migrations/0017_alter_filescript_correction.py
+++ scripts/migrations/0017_alter_filescript_correction.py
@@ -4,15 +4,18 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('scripts', '0016_alter_filescript_correction_and_more'),
+        ("scripts", "0016_alter_filescript_correction_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='filescript',
-            name='correction',
-            field=models.IntegerField(choices=[(0, 'No correction available'), (7001, 'Right to be forgotten'), (8001, 'Account closure')], db_index=True, help_text='Correction Required'),
+            model_name="filescript",
+            name="correction",
+            field=models.IntegerField(
+                choices=[(0, "No correction available"), (7001, "Right to be forgotten"), (8001, "Account closure")],
+                db_index=True,
+                help_text="Correction Required",
+            ),
         ),
     ]
